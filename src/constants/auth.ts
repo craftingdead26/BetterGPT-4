93f47f1fe2bfde5208a38ba2a9961a755a93f72e
craftingdead26 <106111718+craftingdead26@users.noreply.github.com>
2023-10-11 09:59:33 -0500

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,6 +1,6 @@
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://postapi.lbbai.cc/v1/chat/completions';
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://api.aigcfun.com/api/v1/text?key=FCAW3STZXEVXDUJUL8';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || customAPIEndpoint;
 
