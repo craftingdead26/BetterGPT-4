eeec1c51364bfb0a98b905077849a782ca91c3ab
craftingdead26 <106111718+craftingdead26@users.noreply.github.com>
2023-11-20 14:12:03 -0600

    Update auth.ts

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+export const officialAPIEndpoint = 'https://gpt-api.hilbertc.info/v1/chat/completions';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://tan-proxy-7io2un3kuq-uw.a.run.app/v1/';
 export const defaultAPIEndpoint =
